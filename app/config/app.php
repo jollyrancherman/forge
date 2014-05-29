070891139c89d48d040c35224024f5356a83a3bd
jollyrancherman <anthony.rsc@gmail.com>
2014-05-29 03:20:47 -0700

    Revert 47f024d..f38428b
    This rolls back to commit 47f024d0a8a10e4de06b4f35f13d327af6291f77.

--- app/config/app.php
+++ app/config/app.php
@@ -119,7 +119,6 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Way\Generators\GeneratorsServiceProvider',
 
 	),
 
