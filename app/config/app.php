abaa22d4678591d9003a0383d4378ce8a03f92f0
jollyrancherman <anthony.rsc@gmail.com>
2014-06-06 13:50:20 -0700

    take off sentry

--- app/config/app.php
+++ app/config/app.php
@@ -120,7 +120,6 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
-		'Cartalyst\Sentry\SentryServiceProvider',
 
 	),
 
@@ -187,7 +186,6 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-		'Sentry' => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
 
 	),
 
