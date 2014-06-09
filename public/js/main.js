f1b1dd2b3708d3c38cecb3f50114c6ceaff7a4ba
jollyrancherman <anthony.rsc@gmail.com>
2014-06-09 11:44:46 -0700

    update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,3 @@
-/*
- * jQuery File Upload Plugin JS Example 8.9.1
- * https://github.com/blueimp/jQuery-File-Upload
- *
- * Copyright 2010, Sebastian Tschan
- * https://blueimp.net
- *
- * Licensed under the MIT license:
- * http://www.opensource.org/licenses/MIT
- */
-
-/* global $, window */
-
 $(function () {
     'use strict';
 
@@ -30,7 +17,6 @@ $(function () {
             '/cors/result.html?%s'
         )
     );
-
 
     $('#fileupload').fileupload('option', {
         // Enable image resizing, except for Android and Opera,
