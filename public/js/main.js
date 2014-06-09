1070d724ee14bf3b8d146718ce43b6efa777afb8
jollyrancherman <anthony.rsc@gmail.com>
2014-06-09 04:56:49 -0700

    Working Fix for file upload

--- public/js/main.js
+++ public/js/main.js
@@ -31,31 +31,53 @@ $(function () {
         )
     );
 
-    if (window.location.hostname === 'blueimp.github.io') {
-        // Demo settings:
+    // if (window.location.hostname === 'blueimp.github.io') {
+    //     // Demo settings:
+    //     $('#fileupload').fileupload('option', {
+    //         url: '//jquery-file-upload.appspot.com/',
+    //         // Enable image resizing, except for Android and Opera,
+    //         // which actually support image resizing, but fail to
+    //         // send Blob objects via XHR requests:
+    //         disableImageResize: /Android(?!.*Chrome)|Opera/
+    //             .test(window.navigator.userAgent),
+    //     maxNumberOfFiles: 20,
+    //     imageMaxHeight:600,
+    //     imageMaxWidth:600,
+    //     // formData: {example: 'test'},
+    //     imageQuality:0.80,
+    //     maxFileSize: 5000000,
+    //         acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
+    //     });
+    //     // Upload server status check for browsers with CORS support:
+    //     if ($.support.cors) {
+    //         $.ajax({
+    //             url: '//jquery-file-upload.appspot.com/',
+    //             type: 'HEAD'
+    //         }).fail(function () {
+    //             $('<div class="alert alert-danger"/>')
+    //                 .text('Upload server currently unavailable - ' +
+    //                         new Date())
+    //                 .appendTo('#fileupload');
+    //         });
+    //     }
+    // } else {
+
+
         $('#fileupload').fileupload('option', {
-            url: '//jquery-file-upload.appspot.com/',
             // Enable image resizing, except for Android and Opera,
             // which actually support image resizing, but fail to
             // send Blob objects via XHR requests:
             disableImageResize: /Android(?!.*Chrome)|Opera/
                 .test(window.navigator.userAgent),
+            maxNumberOfFiles: 20,
+            imageMaxHeight:600,
+            imageMaxWidth:600,
+            // formData: {example: 'test'},
+            imageQuality:0.80,
             maxFileSize: 5000000,
             acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
         });
-        // Upload server status check for browsers with CORS support:
-        if ($.support.cors) {
-            $.ajax({
-                url: '//jquery-file-upload.appspot.com/',
-                type: 'HEAD'
-            }).fail(function () {
-                $('<div class="alert alert-danger"/>')
-                    .text('Upload server currently unavailable - ' +
-                            new Date())
-                    .appendTo('#fileupload');
-            });
-        }
-    } else {
+     
         // Load existing files:
         $('#fileupload').addClass('fileupload-processing');
         $.ajax({
@@ -70,6 +92,6 @@ $(function () {
             $(this).fileupload('option', 'done')
                 .call(this, $.Event('done'), {result: result});
         });
-    }
+    // }
 
 });
