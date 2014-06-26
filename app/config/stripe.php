256e5d64213594b1885bd1c369e53de7e7228c03
jollyrancherman <anthony.rsc@gmail.com>
2014-06-26 11:19:39 -0700

    use test key

--- app/config/stripe.php
+++ app/config/stripe.php
@@ -1,6 +1,10 @@
 <?php
 
+	// return [
+	// 	"secret_key" => "sk_live_YMneBX7kRFbKZNP3YowjanFJ",
+	// 	"publishable_key" => "pk_live_sHhZBAi2KyatkwqSbYRHtXgA"
+	// ];
 	return [
-		"secret_key" => "sk_live_YMneBX7kRFbKZNP3YowjanFJ",
-		"publishable_key" => "pk_live_sHhZBAi2KyatkwqSbYRHtXgA"
+		"secret_key" => "sk_test_oCDweFB3i1Iak14thwum1lhF",
+		"publishable_key" => "pk_test_qT2vg6kvribCo4KHm1uQjpmR"
 	];
